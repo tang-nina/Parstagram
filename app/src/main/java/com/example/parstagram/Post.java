6bb618a76c7fb5a05a0314d3f6877a6aae5a983c
Nina Tang <ntang@fb.com>
2020-07-09 16:55:40 -0400

    Added ability to like a post on the home feed, and have the number of likes display under the post.

--- app/src/main/java/com/example/parstagram/Post.java
+++ app/src/main/java/com/example/parstagram/Post.java
@@ -18,12 +18,16 @@ public class Post extends ParseObject implements Serializable {
     public static final String KEY_USER = "user";
     public static final String KEY_IMAGE = "image";
     public static final String KEY_CREATED = "createdAt";
+    public static final String KEY_LIKES = "likes";
+    public static final String KEY_ID = "objectId";
 
     public Post(){}
 
     public Date getTimestamp(){ return getCreatedAt(); }
 
     public String getDescription(){ return getString(KEY_DESCRIPTION); }
+
+    public String getId(){ return getObjectId(); }
 
     public void setDescription(String description){
         put(KEY_DESCRIPTION, description);
@@ -45,7 +49,6 @@ public class Post extends ParseObject implements Serializable {
         put(KEY_USER, user);
     }
 
-
     public static String getRelativeTimeAgo(String rawJsonDate) {
         String twitterFormat = "EEE MMM dd HH:mm:ss zzz yyyy";
         SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
@@ -60,6 +63,30 @@ public class Post extends ParseObject implements Serializable {
             e.printStackTrace();
         }
         return relativeDate;
+    }
+
+    public int getLikes(){ return (int) getNumber(KEY_LIKES); }
+
+    public void addLike(){
+        System.out.println(getId());
+        put(KEY_LIKES, getLikes()+1);
+        saveInBackground();
+    }
+
+    public void subtractLike(){
+        put(KEY_LIKES, getLikes()-1);
+        saveInBackground();
+    }
+
+    public String formatLikes(){
+        int likes = getLikes();
+        if(likes == 0){
+            return null;
+        }else if(likes == 1){
+            return "1 like";
+        }else{
+            return likes + " likes";
+        }
     }
 
 }
--- app/src/main/java/com/example/parstagram/PostsAdapter.java
+++ app/src/main/java/com/example/parstagram/PostsAdapter.java
@@ -13,8 +13,13 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseFile;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> {
@@ -27,11 +32,28 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
     OnClickListener onClickListener;
     Context context;
     List<Post> posts;
+    ArrayList<String> likes;
 
 public PostsAdapter(Context context, List<Post> posts,  OnClickListener onClickListener){
     this.context = context;
     this.posts = posts;
     this.onClickListener = onClickListener;
+
+    ParseQuery<ParseUser> query = ParseUser.getQuery();
+    query.whereEqualTo("objectId", ParseUser.getCurrentUser().getObjectId());
+    query.include("likedPosts");
+
+    query.findInBackground(new FindCallback<ParseUser>() {
+        @Override
+        public void done(List<ParseUser> objects, ParseException e) {
+            ArrayList temp = (ArrayList) objects.get(0).get("likedPosts");
+            if(temp == null){
+                likes = new ArrayList<String>();
+            }else{
+                likes.addAll(temp);
+            }
+        }
+    });
 }
 
     @NonNull
@@ -65,6 +87,7 @@ public PostsAdapter(Context context, List<Post> posts,  OnClickListener onClickL
     }
 
     class ViewHolder extends RecyclerView.ViewHolder{
+        ParseUser user = ParseUser.getCurrentUser();
 
     LinearLayout llPosts;
     TextView tvUsername;
@@ -73,22 +96,100 @@ public PostsAdapter(Context context, List<Post> posts,  OnClickListener onClickL
     ImageView ivImage;
     ImageView ivProfilePic;
     RelativeLayout rlHeader;
-
+    TextView tvLikes;
+    ImageView ivLike;
+    ImageView ivComment;
+    ImageView ivDirect;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvUsername = itemView.findViewById(R.id.tvUsername);
             tvDescription = itemView.findViewById(R.id.tvDescription);
+            tvLikes = itemView.findViewById(R.id.tvLikes);
             ivImage = itemView.findViewById(R.id.ivImage);
             tvTimestamp = itemView.findViewById(R.id.tvTimestamp);
             ivProfilePic = itemView.findViewById(R.id.ivProfilePic);
+            ivLike = itemView.findViewById(R.id.ivLike);
+            ivComment = itemView.findViewById(R.id.ivComment);
+            ivDirect = itemView.findViewById(R.id.ivDirect);
+
+            ivDirect.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                }
+            });
 
             llPosts = itemView.findViewById(R.id.llPost);
             rlHeader = itemView.findViewById(R.id.rlHeader);
-
         }
 
         public void bind(final Post post) {
+
+            if(likes.contains(post.getId())){
+                System.out.println(post.getId());
+                Glide.with(context).load(R.drawable.ufi_heart_active).into(ivLike);
+                ivLike.setColorFilter(context.getResources().getColor(R.color.red));
+
+                String likes = post.formatLikes();
+                tvLikes.setVisibility(View.VISIBLE);
+                tvLikes.setText(post.formatLikes());
+
+                ivLike.setTag("liked");
+            }else{
+                Glide.with(context).load(R.drawable.ufi_heart).into(ivLike);
+                ivLike.setColorFilter(context.getResources().getColor(R.color.black));
+
+                String likes = post.formatLikes();
+                if(likes == null){
+                    tvLikes.setVisibility(View.GONE);
+                }else{
+                    tvLikes.setVisibility(View.VISIBLE);
+                    tvLikes.setText(post.formatLikes());
+                }
+
+                ivLike.setTag("unliked");
+            }
+
+            ivLike.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(ivLike.getTag().equals("unliked")){
+                        post.addLike();
+                        likes.add(post.getId());
+                        user.put("likedPosts", likes);
+                        user.saveInBackground();
+
+                        Glide.with(context).load(R.drawable.ufi_heart_active).into(ivLike);
+                        ivLike.setColorFilter(context.getResources().getColor(R.color.red));
+
+                        String likes = post.formatLikes();
+                        tvLikes.setVisibility(View.VISIBLE);
+                        tvLikes.setText(post.formatLikes());
+
+                        ivLike.setTag("liked");
+                    }else if(ivLike.getTag().equals("liked")){
+                        post.subtractLike();
+                        likes.remove(post.getId());
+                        user.put("likedPosts", likes);
+                        user.saveInBackground();
+
+                        Glide.with(context).load(R.drawable.ufi_heart).into(ivLike);
+                        ivLike.setColorFilter(context.getResources().getColor(R.color.black));
+
+                        String likes = post.formatLikes();
+                        if(likes == null){
+                            tvLikes.setVisibility(View.GONE);
+                        }else{
+                            tvLikes.setVisibility(View.VISIBLE);
+                            tvLikes.setText(post.formatLikes());
+                        }
+
+                        ivLike.setTag("unliked");
+
+                    }
+                }
+            });
+
 
            llPosts.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -107,6 +208,14 @@ public PostsAdapter(Context context, List<Post> posts,  OnClickListener onClickL
             tvUsername.setText(post.getUser().getUsername());
             tvDescription.setText(post.getDescription());
             tvTimestamp.setText(Post.getRelativeTimeAgo(post.getTimestamp().toString()));
+
+            String likes = post.formatLikes();
+            if(likes == null){
+                tvLikes.setVisibility(View.GONE);
+            }else{
+                tvLikes.setVisibility(View.VISIBLE);
+                tvLikes.setText(post.formatLikes());
+            }
 
             ParseFile image = post.getImage();
             if(image !=null){
